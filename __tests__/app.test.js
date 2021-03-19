f42392c434d93494059c40eac03b4f02abb19206
Annalise-M <nali247@gmail.com>
2021-03-19 14:31:08 -0700

    Insert function first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
 const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+const Menu = require('../lib/models/menu');
+
 
 describe('Menu-Model routes', () => {
+
   beforeEach(() => {
-    return setup(pool);
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('insert a new food item onto the menu database', async() => {
+    const createdMenu = await Menu.insert({
+      item: 'Tiramisu',
+      description: 'An italian coffee custard dessert',
+      price: '$6.70',
+    });
+
+    const { rows } = await pool.query(
+      'SELECT * FROM menus WHERE id = $1',
+      [createdMenu.id]
+    );
+
+    expect(rows[0]).toEqual(createdMenu);
   });
 });

9829d4335d93e57f9bc0ecb7c9bf9e064049daac
Annalise-M <nali247@gmail.com>
2021-03-25 17:45:09 -0700

    POST test passing for ADMIN route

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-// const admin = require('../lib/models/admin');
+const AdminService = require('../lib/services/admin-service');
 
 describe('admin routes', () => {
   beforeEach(() => {
@@ -25,19 +25,24 @@ describe('admin routes', () => {
   });
 
 
-  // it('logs in admin via POST', async() => {
-  //   const response = await request(app)
-  //     .post('/v1/admin/login')
-  //     .send({
-  //       email: 'test@test.com',
-  //       password: 'password1'
-  //     });
+  it('logs in admin via POST', async() => {
+    const admin = await AdminService.create({
+      email: 'test@test.com',
+      password: 'password1'
+    }); 
 
-  //   expect(response.body).toEqual({
-  //     id: expect.any(String),
-  //     email: 'test@test.com'
-  //   });
-  // });
+    const response = await request(app)
+      .post('/api/v1/admin/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password1'
+      });
+
+    expect(response.body).toEqual({
+      id: admin.id,
+      email: 'test@test.com'
+    });
+  });
 
 
 });
